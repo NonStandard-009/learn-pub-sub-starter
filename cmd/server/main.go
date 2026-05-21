fb904484063af40f9c0209f39dbfd83509a938e4
NonStandard-009 <sebaslr.2002@proton.me>
2026-05-21 13:05:35 -0500

    Implemented connection to RabbitMQ

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	conStr := "amqp://guest:guest@localhost:5672/"
+	rabMQcon, err := amqp.Dial(conStr)
+	if err != nil {
+		fmt.Printf("unexpected error: %v", err)
+		return
+	}
+	defer rabMQcon.Close()
+	fmt.Println("Successful connetion to RabbitMQ")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
 }
