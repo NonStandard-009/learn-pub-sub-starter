47939373163e1aaece283e11c1f8a3b82625252b
NonStandard-009 <sebaslr.2002@proton.me>
2026-05-22 18:44:09 -0500

    Implemented server commands

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -28,16 +29,43 @@ func main() {
 		return
 	}
 
-	if err := pubsub.PublishJSON(
-		ch,
-		routing.ExchangePerilDirect,
-		routing.PauseKey,
-		routing.PlayingState{
-			IsPaused: true,
-		},
-	); err != nil {
-		fmt.Printf("unexpected error: %v", err)
-		return
+	gamelogic.PrintServerHelp()
+	for {
+		words := gamelogic.GetInput()
+		if len(words) == 0 {
+			continue
+		}
+		if words[0] == "pause" {
+			fmt.Println("Sending pause message...")
+			err = pubsub.PublishJSON(
+				ch,
+				routing.ExchangePerilDirect,
+				routing.PauseKey,
+				routing.PlayingState{
+					IsPaused: true,
+				},
+			)
+		} else if words[0] == "resume" {
+			fmt.Println("Sending resume message...")
+			err = pubsub.PublishJSON(
+				ch,
+				routing.ExchangePerilDirect,
+				routing.PauseKey,
+				routing.PlayingState{
+					IsPaused: false,
+				},
+			)
+		} else if words[0] == "quit" {
+			fmt.Println("Sending quit message...")
+			return
+		} else {
+			fmt.Println("Command not valid...")
+		}
+
+		if err != nil {
+			fmt.Printf("unexpected error: %v", err)
+			return
+		}
 	}
 
 	signalChan := make(chan os.Signal, 1)
