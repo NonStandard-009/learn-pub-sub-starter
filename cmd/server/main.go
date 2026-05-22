30c6f1a364373f1f64d600d1690e59fc68ea7080
NonStandard-009 <sebaslr.2002@proton.me>
2026-05-22 13:26:59 -0500

    Implemented PublishJSON function

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -19,6 +21,24 @@ func main() {
 	}
 	defer rabMQcon.Close()
 	fmt.Println("Successful connetion to RabbitMQ")
+
+	ch, err := rabMQcon.Channel()
+	if err != nil {
+		fmt.Printf("unexpected error: %v", err)
+		return
+	}
+
+	if err := pubsub.PublishJSON(
+		ch,
+		routing.ExchangePerilDirect,
+		routing.PauseKey,
+		routing.PlayingState{
+			IsPaused: true,
+		},
+	); err != nil {
+		fmt.Printf("unexpected error: %v", err)
+		return
+	}
 
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
